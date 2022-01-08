87d45dae8ab40698b403845b688631b18d85a40d
harsh <aggarwalharsh912@gmail.com>
2022-01-09 01:16:10 +0530

    first commit

--- remotecode_backend/index.js
+++ remotecode_backend/index.js
@@ -1,20 +1,73 @@
-const express = require('express');
-const exec = require('child_process').exec;
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
-const app = express();
+// const express = require('express');
+// const exec = require('child_process').exec;
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const fs = require('fs');
+// const app = express();
 
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+// app.use(cors());
 
 
-app.post('/' , function(req, res){
+// app.post('/' , function(req, res){
     
 
-    fs.writeFile('./c++/test.cpp' , req.body.code ,  function(err){
+//     fs.writeFile('./c++/test.cpp' , req.body.code ,  function(err){
+//         if(err) {
+//             console.log(err)
+//             return;
+//         }
+//         console.log('saved');
+//     });
+
+//     exec('g++ ./c++/test.cpp -o ./c++/a' , function(err, stdout, stderr){
+//         if(err) {
+//             console.log(err)
+//             return;
+//         }
+//         if(stderr) console.log(stderr);
+
+//         exec('./c++/a' , function(err, stdout , stderr){
+//             if(err) {
+//                 console.log(err)
+//                 return;
+//             }
+//             if(stdout) res.json({result: stdout});
+//         })
+//     })
+    
+    
+// });
+
+// app.listen('4000', (err)=>{
+//     console.log('server is up and running');
+// })
+
+const express = require('express');
+const { exec , spawn } = require('child_process');
+const cors = require('cors');
+const fs = require('fs');
+
+
+const app = express();
+
+var code = {
+    code: `#include<iostream>\n using namespace std; \n int main(){ \n int a,b; cin>>a>>b; cout<<a+b;}`
+}
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+const runCode = async (req, res) =>{
+
+    let code = req.body.code;
+    let input = req.body.input;
+
+    console.log(input)
+    fs.writeFile('hello.cpp' ,code,  function(err){
         if(err) {
             console.log(err)
             return;
@@ -22,25 +75,29 @@ app.post('/' , function(req, res){
         console.log('saved');
     });
 
-    exec('g++ ./c++/test.cpp -o a' , function(err, stdout, stderr){
-        if(err) {
-            console.log(err)
+    exec('g++ hello.cpp' , (error , stderr , stdout) =>{
+        if(error){
+            console.log(`err : ${error}`);
             return;
         }
-        if(stderr) console.log(stderr);
 
-        exec('./a' , function(err, stdout , stderr){
-            if(err) {
-                console.log(err)
-                return;
-            }
-            if(stdout) res.json({result: stdout});
-        })
-    })
-    
-    
-});
+        if(stderr){
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
 
-app.listen('4000', (err)=>{
-    console.log('server is up and running');
-})
+        var child = spawn('./a.out');
+        child.on('error' , (err)=>{
+        console.log(err)
+         });
+        child.stdin.write(input);
+        child.stdin.end();
+        child.stdout.setEncoding('utf-8');
+        child.stdout.on('data' , (data)=> console.log(data));
+        child.stderr.on('data' , (data)=> console.log(data));
+    });
+}
+
+
+app.post('/' , runCode);
+app.listen('4000', ()=>{ console.log('listeninggggg')});
--- remotecode_frontend/src/components/index.js
+++ remotecode_frontend/src/components/index.js
@@ -1,51 +1,78 @@
-import React,{Component} from 'react';
-import './index.css';
+import React, { Component } from "react";
+import "./index.css";
 
-class Main extends Component{
-    
-    constructor(){
-        super();
-        this.state = {
-            result : "",
-            code: ""
-        }
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "",
+      code: "",
+      input: "",
+    };
+  }
+
+  handleTab = async (e) => {
+    if (e.keyCode === 9) {
+      e.preventDefault();
+      await this.setState({ code: this.state.code + "    " });
     }
-    
-    handleSubmit(e){
-        e.preventDefault();
-        
-        fetch('http://localhost:4000' , {
-            method: "POST",
-            body:JSON.stringify({
-                code: this.state.code
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        }).then(res=> res.json()).then((json)=>{
-            this.setState({result: json.result});
-        })
-    }
+  };
 
-    render(){
-        return (
-            <div>
-                <form action='http://localhost:4000/' method="POST" onSubmit={async (e) => {
-                    await this.setState({code: e.target.code.value});
-                    this.handleSubmit(e);
-                }}>
-                    <textarea id='textarea' name="code" ></textarea>
-                    <button type="submit">Submit</button>
-                </form>
+  handleSubmit(e) {
+    e.preventDefault();
 
-                <div class="result">
-                    <p>{this.state.result}</p>
-                </div>
-            </div>
-        )
-    }
-    
-};
+    fetch("http://localhost:4000", {
+      method: "POST",
+      body: JSON.stringify({
+        code: this.state.code,
+        input: this.state.input,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ result: json.result });
+      });
+  }
 
+  render() {
+    return (
+      <div>
+        <form
+          action="http://localhost:4000/"
+          method="POST"
+          onSubmit={async (e) => {
+            await this.setState({ code: e.target.code.value });
+            this.handleSubmit(e);
+          }}
+        >
+          <textarea
+            value={this.state.code}
+            id="textarea"
+            name="code"
+            onKeyDown={this.handleTab}
+            onChange={async (e) => {
+              await this.setState({ code: e.target.value });
+            }}
+          ></textarea>
+          <input
+            value={this.state.input}
+            name="input"
+            onChange={async (e) => {
+              await this.setState({ input: e.target.value });
+            }}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+
+        <div class="result">
+          <p>{this.state.result}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Main;
